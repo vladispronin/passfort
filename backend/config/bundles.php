b8f24d413d5becfc95104c78a5ff8c97bfb3a335
vladispronin <93580351+vladispronin@users.noreply.github.com>
2026-03-17 13:33:40 +0200

    add logging + xdebug

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Koco\Kafka\KocoKafkaBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
