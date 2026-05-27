311bca5ea5dd4b20729c5130bf38a8a13daefa92
vladispronin <93580351+vladispronin@users.noreply.github.com>
2026-05-27 19:47:34 +0200

    ci frontend fix

--- frontend/tests/unit/crypto/kdf.test.ts
+++ frontend/tests/unit/crypto/kdf.test.ts
@@ -39,7 +39,7 @@ describe('KDF Module', () => {
         }),
         mockKey,
         { name: 'AES-GCM', length: 256 },
-        false,
+        true,
         ['encrypt', 'decrypt'],
       )
     })
